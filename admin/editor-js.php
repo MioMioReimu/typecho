950fb385e642dea1c99d1bf91985931c1cd1c389
tlm <tlm19920920@sina.com>
2015-07-20 15:09:42 +0800

    Changes to be committed: 	modified:   admin/editor-js.php

--- admin/editor-js.php
+++ admin/editor-js.php
@@ -74,7 +74,7 @@ $(document).ready(function () {
     {
         var s = "";
         if (str.length == 0) return "";
-        s = str.replace(/&/g, "&gt;");
+        s = str.replace(/&/g, "&amp;");
         s = s.replace(/</g, "&lt;");
         s = s.replace(/>/g, "&gt;");
         s = s.replace(/ /g, "&nbsp;");
@@ -88,7 +88,7 @@ $(document).ready(function () {
     {
         var s = "";
         if (str.length == 0) return "";
-        s = str.replace(/&gt;/g, "&");
+        s = str.replace(/&amp;/g, "&");
         s = s.replace(/&lt;/g, "<");
         s = s.replace(/&gt;/g, ">");
         s = s.replace(/&nbsp;/g, " ");
